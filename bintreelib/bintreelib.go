cccda3d8dc2c53fbc2d236ef332d93db520b5573
Neehar Patil <pluckynumbat@gmail.com>
2025-01-25 11:48:06 +0530

    added required imports and some basic error messages

--- bintreelib/bintreelib.go
+++ bintreelib/bintreelib.go
@@ -1,6 +1,15 @@
 // Package bintreelib: Basic Binary Tree Stuff
 package bintreelib
 
+import (
+	"fmt"
+
+	"github.com/pluckynumbat/go-quez/sgquezlib"
+)
+
+var treeNilError = fmt.Errorf("the binary tree is nil")
+var rootNilError = fmt.Errorf("the root is nil")
+
 type Node struct {
 	left  *Node
 	data  string
