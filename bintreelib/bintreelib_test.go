f86a38e4af0b2f926706cef4d894fae21389df19
Neehar Patil <pluckynumbat@gmail.com>
2025-01-27 11:27:26 +0530

    made the node string tests table driven

--- bintreelib/bintreelib_test.go
+++ bintreelib/bintreelib_test.go
@@ -5,25 +5,28 @@ import (
 )
 
 func TestNodeString(t *testing.T) {
-	var node *Node
-	want := "nil"
-	got := node.String()
+	var n1, n2, n3 *Node
 
-	if got != want {
-		t.Errorf("String() returned incorrect results, want: %v, got %v", want, got)
+	n2 = &Node{}
+	n3 = &Node{nil, "a", nil}
+
+	tests := []struct {
+		name string
+		node *Node
+		want string
+	}{
+		{"nil node", n1, "nil"},
+		{"non nil empty node", n2, ""},
+		{"non nil non empty node", n3, "a"},
 	}
 
-	node = &Node{}
-	want = ""
-	got = node.String()
-	if got != want {
-		t.Errorf("String() returned incorrect results, want: %v, got %v", want, got)
-	}
-
-	node = &Node{nil, "a", nil}
-	want = "a"
-	got = node.String()
-	if got != want {
-		t.Errorf("String() returned incorrect results, want: %v, got %v", want, got)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got := test.node.String()
+			want := test.want
+			if got != want {
+				t.Errorf("String() returned incorrect results, want: %v, got %v", want, got)
+			}
+		})
 	}
 }
