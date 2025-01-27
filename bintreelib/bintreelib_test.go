831d729ebb57cd96aae29de71feb31fcc9c2d858
Neehar Patil <pluckynumbat@gmail.com>
2025-01-27 11:23:21 +0530

    added the tests file for the binary tree library, added a basic node string test to it with a nil node

---
+++ bintreelib/bintreelib_test.go
@@ -0,0 +1,15 @@
+package bintreelib
+
+import (
+	"testing"
+)
+
+func TestNodeString(t *testing.T) {
+	var node *Node
+	want := "nil"
+	got := node.String()
+
+	if got != want {
+		t.Errorf("String() returned incorrect results, want: %v, got %v", want, got)
+	}
+}
