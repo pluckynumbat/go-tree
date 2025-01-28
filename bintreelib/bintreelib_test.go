582d2447cb72360ea71e154629cfe83aeea59fc0
Neehar Patil <pluckynumbat@gmail.com>
2025-01-28 10:34:40 +0530

    added table driven tests for the Root() method on the binary tree

--- bintreelib/bintreelib_test.go
+++ bintreelib/bintreelib_test.go
@@ -83,3 +83,42 @@ func TestIsEmpty(t *testing.T) {
 	}
 }
 
+func TestRoot(t *testing.T) {
+
+	var bt1, bt2, bt3, bt4, bt5 *BinaryTree
+	bt2 = &BinaryTree{}
+
+	r1 := &Node{nil, "1", nil}
+	bt3 = &BinaryTree{r1}
+
+	n2 := &Node{nil, "b", nil}
+	r2 := &Node{n2, "a", nil}
+	bt4 = &BinaryTree{r2}
+
+	n3 := &Node{nil, "l", nil}
+	n4 := &Node{nil, "r", nil}
+	r3 := &Node{n3, "m", n4}
+	bt5 = &BinaryTree{r3}
+
+	tests := []struct {
+		name string
+		bt   *BinaryTree
+		want string
+	}{
+		{"nil binary tree", bt1, "nil"},
+		{"empty binary tree", bt2, "nil"},
+		{"1 element binary tree", bt3, "1"},
+		{"2 element binary tree", bt4, "a"},
+		{"3 element binary tree", bt5, "m"},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			want := test.want
+			got := test.bt.Root().String()
+			if got != want {
+				t.Errorf("Root() returned incorrect results, want: %v, got %v", want, got)
+			}
+		})
+	}
+}
