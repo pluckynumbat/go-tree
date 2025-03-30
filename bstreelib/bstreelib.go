4e0db19bc8e87592b2c993a2dbada4f5a166c081
Neehar Patil <pluckynumbat@gmail.com>
2025-03-30 13:25:54 +0530

    added the node for the binary search tree, which can contain data of any type which implements the comprable interface

--- bstreelib/bstreelib.go
+++ bstreelib/bstreelib.go
@@ -8,3 +8,11 @@ import (
 var nodeNilError = fmt.Errorf("the node is nil")
 var treeNilError = fmt.Errorf("the binary search tree is nil")
 var treeEmptyError = fmt.Errorf("the binary search tree is empty")
+
+// Node is the basic unit of the binary search tree, and contains data which can be anything that implements the comparable interface
+type Node[T comparable] struct {
+	data   T
+	parent *Node[T]
+	left   *Node[T]
+	right  *Node[T]
+}
