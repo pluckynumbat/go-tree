72fe21bb7f4b38423bc8fe42ef164dfe451d496e
Neehar Patil <pluckynumbat@gmail.com>
2025-03-30 13:25:07 +0530

    added standard error messages for the binary search tree

--- bstreelib/bstreelib.go
+++ bstreelib/bstreelib.go
@@ -1,2 +1,10 @@
 // Package bstreelib: Basic Binary Search Tree Stuff
 package bstreelib
+
+import (
+	"fmt"
+)
+
+var nodeNilError = fmt.Errorf("the node is nil")
+var treeNilError = fmt.Errorf("the binary search tree is nil")
+var treeEmptyError = fmt.Errorf("the binary search tree is empty")
