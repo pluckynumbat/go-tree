9825f4954edb79153caddcab5313c12702bab6c0
Neehar Patil <pluckynumbat@gmail.com>
2025-03-31 14:22:12 +0530

    added a basic test for the node string function to the binary search tree test file

--- bstreelib/bstreelib_test.go
+++ bstreelib/bstreelib_test.go
@@ -1,8 +1,22 @@
 package bstreelib
 
-import "fmt"
+import (
+	"fmt"
+	"testing"
+)
 
 type prInt int // printable int
 func (p prInt) String() string {
 	return fmt.Sprintf("%v", int(p))
+}
+
+func TestNodeString(t *testing.T) {
+	node := &Node[prInt]{1, nil, nil, nil}
+
+	want := "1"
+	got := node.String()
+
+	if got != want {
+		t.Errorf("Node's string returned incorrect results, want: %v, got %v", want, got)
+	}
 }
