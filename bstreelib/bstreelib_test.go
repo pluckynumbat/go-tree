3a22180eabcde5fab9dd4624560e1aff9a66acd9
Neehar Patil <pluckynumbat@gmail.com>
2025-04-04 13:05:01 +0530

    added table driven tests for the left child method on the node of type prString

--- bstreelib/bstreelib_test.go
+++ bstreelib/bstreelib_test.go
@@ -142,6 +142,40 @@ func TestNodeLeftChild(t *testing.T) {
 			})
 		}
 	})
+
+	t.Run("test node left child: prString", func(t *testing.T) {
+		var n1, n2, n3 *Node[prString]
+		n2 = &Node[prString]{"a", nil, nil, nil}
+		n3 = &Node[prString]{"b", n2, nil, nil}
+		n2.left = n3
+
+		tests := []struct {
+			name         string
+			node         *Node[prString]
+			expError     error
+			leftChild    *Node[prString]
+			leftChildStr string
+		}{
+			{"nil node", n1, nodeNilError, nil, "nil"},
+			{"non nil node, nil left child", n3, nil, nil, "nil"},
+			{"non nil node, non nil left child", n2, nil, n3, "b"},
+		}
+
+		for _, test := range tests {
+			t.Run(test.name, func(t *testing.T) {
+				leftChild, err := test.node.LeftChild()
+				if err != nil {
+					if !errors.Is(err, test.expError) {
+						t.Fatalf("LeftChild() failed with unexpected error: %v", err)
+					}
+				} else if leftChild != test.leftChild {
+					t.Fatalf("LeftChild() returned incorrect left child pointer, want: %v, got: %v", test.leftChild, leftChild)
+				} else if leftChild.String() != test.leftChildStr {
+					t.Errorf("LeftChild() returned left child pointer with incorrect string, want: %v, got: %v", test.leftChildStr, leftChild.String())
+				}
+			})
+		}
+	})
 }
 
 func TestNodeRightChild(t *testing.T) {
