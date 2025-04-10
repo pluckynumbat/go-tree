5ef8d9f8d757aa31f73cb2e245aa9075d1b0ec54
Neehar Patil <pluckynumbat@gmail.com>
2025-04-10 15:27:58 +0530

    update the summary comment for the construct from values function

--- bstreelib/bstreelib.go
+++ bstreelib/bstreelib.go
@@ -124,7 +124,7 @@ func (bst *BinarySearchTree[T]) Insert(value T) error {
 	return nil
 }
 
-// ConstructFromValues is a helper function to add all the given values (in the order that they are provided) to a binary search tree
+// ConstructFromValues is a helper function to insert all the given values (in the order that they are provided) into a binary search tree
 func ConstructFromValues[T BinarySearchTreeElement](values ...T) (*BinarySearchTree[T], error) {
 	bstree := &BinarySearchTree[T]{}
 
