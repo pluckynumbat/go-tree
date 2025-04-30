949c88954e8c835b4d2032587ae0cb1b19799e0e
Neehar Patil <pluckynumbat@gmail.com>
2025-04-30 16:14:12 +0530

    added nil tree test case for the Search method of prfloat

--- bstreelib/bstreelib_test.go
+++ bstreelib/bstreelib_test.go
@@ -1477,5 +1477,12 @@ func TestSearch(t *testing.T) {
 	})
 
 	t.Run("Search prFloat", func(t *testing.T) {
+		var bst *BinarySearchTree[prFloat]
+		_, err := bst.Search(0.5)
+		if err == nil {
+			t.Fatalf("Search() on a nil tree should have failed")
+		} else {
+			fmt.Println(err)
+		}
 	})
 }
