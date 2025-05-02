bb943f491d7f3d655c89d52258d2b0cb15385039
Neehar Patil <pluckynumbat@gmail.com>
2025-05-02 15:55:36 +0530

    skeleton method for traversing a binary search tree in a post order manner

--- bstreelib/bstreelib.go
+++ bstreelib/bstreelib.go
@@ -233,6 +233,9 @@ func recurseDFSPreOrder[T BinarySearchTreeElement](node *Node[T]) string {
 	return result
 }
 
+func (bst *BinarySearchTree[T]) TraverseDFSPostOrder() (string, error) {
+}
+
 // Search looks for a given value the binary search tree, and tell you whether that value is present in the tree or not
 func (bst *BinarySearchTree[T]) Search(val T) (bool, error) {
 
