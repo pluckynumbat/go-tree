80c38ce581d100c90e0730f6b3effdd68c588e1b
Neehar Patil <pluckynumbat@gmail.com>
2025-05-24 12:42:36 +0530

    corrected a test for  prString BalanceTree()

--- bstreelib/bstreelib_test.go
+++ bstreelib/bstreelib_test.go
@@ -2105,7 +2105,7 @@ func TestBalanceTree(t *testing.T) {
 
 			{"3 elements: I, me, myself", []prString{"I", "me", "myself"}, "-(me)--(I)--(myself)-"},
 			{"3 elements: I, myself, me", []prString{"I", "myself", "me"}, "-(me)--(I)--(myself)-"},
-			{"3 elements: me, I, myself", []prString{"me", "myself", "I"}, "-(me)--(I)--(myself)-"},
+			{"3 elements: me, I, myself", []prString{"me", "I", "myself"}, "-(me)--(I)--(myself)-"},
 			{"3 elements: me, myself, I", []prString{"me", "myself", "I"}, "-(me)--(I)--(myself)-"},
 			{"3 elements: myself, I, me", []prString{"myself", "I", "me"}, "-(me)--(I)--(myself)-"},
 			{"3 elements: myself, me, I", []prString{"myself", "me", "I"}, "-(me)--(I)--(myself)-"},
