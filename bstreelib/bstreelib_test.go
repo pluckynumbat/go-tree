c6084d8242ec4f861075cad07a582bd1f2a51555
Neehar Patil <pluckynumbat@gmail.com>
2025-05-30 10:40:26 +0530

    start adding the test code for the prInt ConstructBalancedTree() table driven tests

--- bstreelib/bstreelib_test.go
+++ bstreelib/bstreelib_test.go
@@ -2224,6 +2224,21 @@ func TestConstructBalancedTree(t *testing.T) {
 			expDFSInOrderStr   string
 			expDFSPreOrderStr  string
 			expDFSPostOrderStr string
-		}{}
+		}{
+
+		}
+
+		for _, test := range tests {
+			t.Run(test.name, func(t *testing.T) {
+				bst, err := ConstructBalancedTree[prInt](test.input...)
+				if err != nil {
+					if errors.Is(err, test.expError) {
+						fmt.Println(err)
+					} else {
+						t.Fatalf("ConstructBalancedTree() failed with an unexpected error, %v", err)
+					}
+				}
+			})
+		}
 	})
 }
