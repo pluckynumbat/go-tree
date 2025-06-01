deacdc24c4413f44bfd06bedb82cc42b7d7c951b
Neehar Patil <pluckynumbat@gmail.com>
2025-06-01 11:36:02 +0530

    fixed the tree being used in 2 traversal checks done as part of the ConstructBalancedTree() prInt table driven tests

--- bstreelib/bstreelib_test.go
+++ bstreelib/bstreelib_test.go
@@ -2254,14 +2254,14 @@ func TestConstructBalancedTree(t *testing.T) {
 						t.Errorf("DFS In Order traversal results are incorrect, want: %v, got %v", test.expDFSInOrderStr, gotDFSInOrder)
 					}
 
-					gotDFSPreOrder, err2 := bst1.TraverseDFSPreOrder()
+					gotDFSPreOrder, err2 := bst.TraverseDFSPreOrder()
 					if err2 != nil {
 						t.Fatalf("TraverseDFSPreOrder() failed with an unexpected error, %v", err2)
 					} else if gotDFSPreOrder != test.expDFSPreOrderStr {
 						t.Errorf("DFS Pre Order tree traversal results are incorrect, want: %v, got %v", test.expDFSPreOrderStr, gotDFSPreOrder)
 					}
 
-					gotDFSPostOrder, err2 := bst1.TraverseDFSPostOrder()
+					gotDFSPostOrder, err2 := bst.TraverseDFSPostOrder()
 					if err2 != nil {
 						t.Fatalf("TraverseDFSPostOrder() failed with an unexpected error, %v", err2)
 					} else if gotDFSPostOrder != test.expDFSPostOrderStr {
