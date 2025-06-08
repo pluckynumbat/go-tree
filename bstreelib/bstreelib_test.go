147152c71e6f787bd6fae12505244b2b81c24eca
Neehar Patil <pluckynumbat@gmail.com>
2025-06-08 11:26:19 +0530

    more permutations added for the  3 element test case for the  prString ConstructBalancedTree() table driven tests

--- bstreelib/bstreelib_test.go
+++ bstreelib/bstreelib_test.go
@@ -2353,6 +2353,8 @@ func TestConstructBalancedTree(t *testing.T) {
 
 			{"3 elements: I, me, myself", []prString{"I", "me", "myself"}, nil, "-(me)--(I)--(myself)-", "-(I)--(me)--(myself)-", "-(me)--(I)--(myself)-", "-(I)--(myself)--(me)-"},
 			{"3 elements: I, myself, me", []prString{"I", "myself", "me"}, nil, "-(me)--(I)--(myself)-", "-(I)--(me)--(myself)-", "-(me)--(I)--(myself)-", "-(I)--(myself)--(me)-"},
+			{"3 elements: me, I, myself", []prString{"me", "I", "myself"}, nil, "-(me)--(I)--(myself)-", "-(I)--(me)--(myself)-", "-(me)--(I)--(myself)-", "-(I)--(myself)--(me)-"},
+			{"3 elements: me, myself, I", []prString{"me", "myself", "I"}, nil, "-(me)--(I)--(myself)-", "-(I)--(me)--(myself)-", "-(me)--(I)--(myself)-", "-(I)--(myself)--(me)-"},
 		}
 
 		for _, test := range tests {
